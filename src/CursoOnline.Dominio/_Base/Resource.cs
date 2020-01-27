799353418e6c512f708e20e28982c1b4eb20b0d7
fh.proger <phillipe.souza@rte.com.br>
2020-01-26 22:53:16 -0300

    Aula 31 - Organização das mensagens de erro

---
+++ src/CursoOnline.Dominio/_Base/Resource.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CursoOnline.Dominio._Base
+{
+	public class Resource
+	{
+		public static readonly string NomeInvalido = "Nome inválido";
+		public static readonly string CargaHorariaInvalida = "Carga Horária inválido";
+		public static readonly string ValorInvalido = "Valor inválido";
+	}
+}
